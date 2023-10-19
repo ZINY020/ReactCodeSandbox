325472ae61679087c1e27c8e37b994011ea55bdb
ZIN <133877245+ZINY020@users.noreply.github.com>
2023-10-19 13:41:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="ziny">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>너무 신난다야</h2>
     </div>
   );
 }
